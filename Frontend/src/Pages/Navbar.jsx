6d928767a252cf4e7842bc32aa413544ba3ec116
RonakSurana-2001 <ronaksurana2017@gmail.com>
2024-05-26 18:16:45 +0530

    Edited Navbar

--- Frontend/src/Pages/Navbar.jsx
+++ Frontend/src/Pages/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
         <>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <Link to="http://localhost:5173/dnsEntry" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Link to="/dnsEntry" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DNS Manager</span>
                     </Link>
                     <div className="w-auto md:block md:w-auto" id="navbar-default">
