584b78f2bcecdbfc3c391cfaa8f6526b0aafcb07
RonakSurana-2001 <ronaksurana2017@gmail.com>
2024-05-26 15:40:03 +0530

    Added Loader Feature to Login and Register

--- Frontend/src/Pages/LoginPage.jsx
+++ Frontend/src/Pages/LoginPage.jsx
@@ -3,20 +3,23 @@ import React, { useState } from 'react'
 import axios from 'axios';
 // import loginContext from '../context/loginContext'
 import "../../Styles/registerPage.css"
-import { Link,useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast';
-import {  useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { authActions } from "../redux/store";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
+import ReactLoading from 'react-loading';
 
 
 function LoginPage() {
 
   const navigate = useNavigate()
 
-  const dispatch=useDispatch()
+  const dispatch = useDispatch()
 
-  const isLogin=useSelector((state)=>state.isLogin)
+  const isLogin = useSelector((state) => state.isLogin)
+
+  const [loading, setloading] = useState(false);
 
   const [userInfo, setuserInfo] = useState({
     email: "",
@@ -33,46 +36,69 @@ function LoginPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setloading(true)
     try {
       const { data } = await axios.post("https://dns-manager-s2o7.onrender.com/user/userLogin", {
         email: userInfo.email,
         password: userInfo.password,
       })
-      
+
       if (data.success) {
         localStorage.setItem('token', data.jwtToken)
         toast.success("Login Successfully")
         dispatch(authActions.login())
         navigate('/dnsEntry')
+        setloading(false)
       }
-      else{
+      else {
         toast.error('Invalid Username or Password')
         console.log(error)
+        setloading(false)
       }
     } catch (error) {
-      toast.error('Invalid Username or Password')
+      toast.error('Unable to login In')
       console.log(error)
+      setloading(false)
     }
   }
 
   return (
-    <>
-      <div className='register-container'>
+    <div className='register-container'>
+      {!loading && (
         <div className='register-container-circle'>
           <div className='register-container-header'>
             LOGIN
           </div>
           <div className='register-container-main'>
             <form onSubmit={handleSubmit}>
-              <input type="email" name="email" placeholder='Email' value={userInfo.email} onChange={handleChange} style={{width:"70%"}}/>
-              <input type="password" name="password" placeholder='Password' value={userInfo.password} onChange={handleChange} style={{width:"70%"}}/>
+              <input
+                type="email"
+                name="email"
+                placeholder='Email'
+                value={userInfo.email}
+                onChange={handleChange}
+                style={{ width: "70%" }}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder='Password'
+                value={userInfo.password}
+                onChange={handleChange}
+                style={{ width: "70%" }}
+              />
               <button type='submit'>Submit</button>
             </form>
-            <p>NOT A USER ? PLEASE <Link to="/register">REGISTER</Link></p>
+            <p>
+              NOT A USER? PLEASE <Link to="/register">REGISTER</Link>
+            </p>
           </div>
         </div>
-      </div>
-    </>
+      )}
+      {loading && (
+        <ReactLoading type={"spin"} color={"blue"} height={50} width={200} />
+      )}
+    </div>
   )
 }
 
--- Frontend/src/Pages/RegisterPage.jsx
+++ Frontend/src/Pages/RegisterPage.jsx
@@ -1,67 +1,77 @@
 import React, { useState } from 'react'
 import "../../Styles/registerPage.css"
-import { Link,useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 // import loginContext from '../context/loginContext'
 // import { useContext } from 'react';
 import toast from 'react-hot-toast';
+import ReactLoading from 'react-loading';
+
 
 function RegisterPage() {
-    
-    const navigate=useNavigate()
+
+    const [loading, setloading] = useState(false);
+
+    const navigate = useNavigate()
     // const { setIslogin } = useContext(loginContext);
-    const [userInfo,setuserInfo]=useState({
-        username:"",
-        email:"",
-        password:""
+    const [userInfo, setuserInfo] = useState({
+        username: "",
+        email: "",
+        password: ""
     })
 
-    const handleChange=(e)=>{
-        setuserInfo((prevState)=>({
+    const handleChange = (e) => {
+        setuserInfo((prevState) => ({
             ...prevState,
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
         }))
     }
 
-    const handleSubmit=async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
-            const {data}=await axios.post("https://dns-manager-s2o7.onrender.com/user/userRegister",{
-                username:userInfo.username,
-                email:userInfo.email,
-                password:userInfo.password,
+        setloading(true)
+        try {
+            const { data } = await axios.post("https://dns-manager-s2o7.onrender.com/user/userRegister", {
+                username: userInfo.username,
+                email: userInfo.email,
+                password: userInfo.password,
             })
-            if(data.success){
+            if (data.success) {
                 // setIslogin(true)
                 toast.success('User Successfully Registered')
                 navigate('/')
+                setloading(false)
             }
-        } catch(error){
+        } catch (error) {
             toast.error('User Already Registered')
             console.log(error)
+            setloading(false)
         }
     }
 
-  return (
-    <>
-    <div className='register-container'>
-        <div className='register-container-circle'>
-            <div className='register-container-header'>
-                REGISTER
+    return (
+        <>
+            <div className='register-container'>
+                {loading == false && (<div className='register-container-circle'>
+                    <div className='register-container-header'>
+                        REGISTER
+                    </div>
+                    <div className='register-container-main'>
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" name="username" placeholder='Username' value={userInfo.username} onChange={handleChange} style={{ width: "70%" }} />
+                            <input type="email" name="email" placeholder='Email' value={userInfo.email} onChange={handleChange} style={{ width: "70%" }} />
+                            <input type="password" name="password" placeholder='Password' value={userInfo.password} onChange={handleChange} style={{ width: "70%" }} />
+                            <button type='submit'>Submit</button>
+                        </form>
+                        <p>ALREADY REGISTERED ? PLEASE <Link to="/">LOGIN</Link></p>
+                    </div>
+                </div>)}
+                {loading && (
+                    <ReactLoading type={"spin"} color={"blue"} height={50} width={200} />
+                )}
             </div>
-            <div className='register-container-main'>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="username" placeholder='Username' value={userInfo.username} onChange={handleChange} style={{width:"70%"}}/>
-                    <input type="email" name="email" placeholder='Email' value={userInfo.email} onChange={handleChange} style={{width:"70%"}}/>
-                    <input type="password" name="password" placeholder='Password' value={userInfo.password} onChange={handleChange} style={{width:"70%"}}/>
-                    <button type='submit'>Submit</button>
-                </form>
-                <p>ALREADY REGISTERED ? PLEASE <Link to="/">LOGIN</Link></p>
-            </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default RegisterPage
